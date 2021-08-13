67890e9292823ff3cd8c4b6641abdb67333b9d4e
Sano <wkeifreire@gmail.com>
2021-08-13 15:04:47 -0400

    kill me

--- run_createDataset.sh
+++ run_createDataset.sh
@@ -9,6 +9,6 @@ pip install ipython
 jupyter nbconvert --to python JDatasetCreation.ipynb
 jupyter nbconvert --to python Dataset_Creator.ipynb
 
-ipython Dataset_Creator.py -p 50
+ipython -i Dataset_Creator.py -- -p=50
 
 pause 'Press [Enter] key to continue...'
