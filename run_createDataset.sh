64e23f972bcf2e998f3392e49651b978a35125a0
Sano <wkeifreire@gmail.com>
2021-08-13 08:32:05 -0400

    fixing stuff

--- run_createDataset.sh
+++ run_createDataset.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# init
+function pause(){
+   read -p "$*"
+}
+
+pip install ipython
 jupyter nbconvert --to python JDatasetCreation.ipynb
 jupyter nbconvert --to python Dataset_Creator.ipynb
 
-python Dataset_Creator.py -p 50
+ipython Dataset_Creator.py -p 50
+
+pause 'Press [Enter] key to continue...'
